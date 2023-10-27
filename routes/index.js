b4cfd6feb5c36731771bc033a11c88155c9430a6
Skr299 <S567427@nwmissouri.edu>
2023-10-27 09:01:36 -0500

    made changes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("This page is working as expected");
   res.render('index', { title: 'Express' });
 });
 
